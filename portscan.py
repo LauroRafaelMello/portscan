e07d1c2010acc416cbd20c8c4888778ab88694bc
Lauro Mello <laurorafa97@gmail.com>
2023-10-04 22:37:22 -0300

    first commit

---
+++ portscan.py
@@ -0,0 +1,6 @@
+import socket, sys
+for porta in range(1, 65535):
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  if s.connect_ex((sys.argv[1], porta)) == 0:
+    print("Porta", porta, "[ABERTA]")
+    s.close()
