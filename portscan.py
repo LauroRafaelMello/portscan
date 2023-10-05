eb7d657a58a77384a0595bca35fe1f2f32bcb866
Lauro Mello <laurorafa97@gmail.com>
2023-10-05 20:50:43 -0300

    add verbose and timeout

--- portscan.py
+++ portscan.py
@@ -1,6 +1,12 @@
 import socket, sys
 for porta in range(1, 65535):
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.settimeout(0.04)
+  
+  if len(sys.argv) >= 3:
+    if sys.argv[2] == "-v":
+      print("Verificando porta: ", porta, "...")
+  
   if s.connect_ex((sys.argv[1], porta)) == 0:
-    print("Porta", porta, "[ABERTA]")
+    print("=====Porta", porta, "[ABERTA]=====")
     s.close()
